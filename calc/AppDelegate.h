59986910e2265647ccb96738eecfc9303c448e93
tujiaw <tujiaw@192.168.239.129>
2013-11-17 11:17:28 +0800

    Initial Commit

---
+++ calc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  calc
+//
+//  Created by tujiaw on 13-11-17.
+//  Copyright (c) 2013年 tujiaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ calc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  calc
+//
+//  Created by tujiaw on 13-11-17.
+//  Copyright (c) 2013年 tujiaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
